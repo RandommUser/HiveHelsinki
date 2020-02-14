502ce173ef8c9cf1ea83b130e554b428b82cd0e0
Petrus Hakakoski <phakakos@c2r2p2.hive.fi>
2020-02-14 20:17:19 +0200

    cleaned up output and different compile for .a output files

--- helper-scripts/makeme.sh
+++ helper-scripts/makeme.sh
@@ -6,7 +6,7 @@
 #    By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/14 13:52:58 by phakakos          #+#    #+#              #
-#    Updated: 2020/02/14 18:32:31 by phakakos         ###   ########.fr        #
+#    Updated: 2020/02/14 20:14:55 by phakakos         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,20 +14,26 @@
 # Auto solves .c, .a and folders into right categories
 # Made by phakakos, Hive Helsink 2020
 
-CNAME="SRCS"
-ONAME="BIGO"
-INCNAME="INC"
-LIBNAME="LIB"
-FLAGS="-Wall -Wextra -Werror"
 
-NAME=""
-CFILES=""
-OFILES="\$($CNAME:.c=.o)"
-LIB=""
-INCLUDES=""
-EMAKE=""
+# Set the names of the different variables in the Makefile
+CNAME="SRCS"		# all the .c files
+ONAME="BIGO"		# all the .o files
+INCNAME="INC"		# include folders
+LIBNAME="LIB"		# library folders
 
+# Parse parameters
+FLAGS="-Wall -Wextra -Werror"	# flags
+NAME=""							# output name
+CFILES=""						# .c files
+OFILES="\$($CNAME:.c=.o)"		# set the .c files into .o files
+LIB=""							# libraries
+INCLUDES=""						# included folders
+EMAKE=""						# make commands to compile libraries
+
+
+# Create the makefile
 print_make(){
+# move old makefile to Makefile1
 if [[ -f Makefile ]]
 then mv Makefile Makefile1
 fi
@@ -42,7 +48,12 @@ echo "" >> Makefile
 echo "\$(NAME) :" >> Makefile
 echo "$EMAKE"  >> Makefile
 echo "\tgcc -c $FLAGS \$($CNAME) \$($INCNAME)" >> Makefile
-echo "\tgcc $FLAGS -o \$(NAME) \$($ONAME) \$($LIBNAME)" >> Makefile
+TEST=$(echo "$NAME" | grep "\.a$")
+if [[ $TEST == "" ]]						# if the desired output file is an .a file, change the final compilation
+then echo "\tgcc $FLAGS -o \$(NAME) \$($ONAME) \$($LIBNAME)" >> Makefile
+else 
+	echo "\tar rc \$(NAME) \$($ONAME)" >> Makefile; echo "\tranlib \$(NAME)" >> Makefile
+fi
 echo "" >> Makefile
 echo "clean :" >> Makefile
 echo "\trm -f \$($ONAME)" >> Makefile
@@ -54,13 +65,14 @@ echo "re: fclean all" >> Makefile
 echo "Makefile created"; exit
 }
 
-
+# usage
 if [[ $1 == "" ]]
 then echo "usage 'makeme.sh' 'program name' source_file.c/a source_folder"
 	echo "Support direct link to .a library or just the folder. Include folders will be included with just a folder"
 	exit
 fi
 
+# Loop thru all the parameters
 for arg in "$@"
 do
 	TEST=""
@@ -82,7 +94,7 @@ do
 			if [[ $TEST  != "" ]]							# The Makefile compiles an .a file, add it to EMAKE to make before compling
 			then 
 				TEST=$(echo "$arg" | grep "^[\.]\{1,2\}\/\|^~\/")
-				if [[ $TEST != "" ]]						# if the arg has './' , '../'  or '~/' at the of the argument, otherwise at "./"
+				if [[ $TEST != "" ]]						# if the arg has './' , '../'  or '~/' at the of the argument, otherwise add "./"
 				then LIB+="-L $arg "; EMAKE+="\tmake -C $arg\n";
 				else
 					LIB+="-L ./$arg "; EMAKE+="\tmake -C ./$arg\n";
@@ -99,28 +111,18 @@ do
 	then
 		if [[ -f $arg ]]						# it exists
 		then 
-			TEST=$(echo "$arg" | grep "^[\.]\{1,2\}\/\|^~\/")		# starts with './', '../' or '~/'
-			if [[ $TEST == "" ]]				# add the start ^
+			TEST=$(echo "$arg" | grep "^[\.]\{1,2\}\/\|^~\/")						# starts with './', '../' or '~/'
+			if [[ $TEST == "" ]]													# add the start ^ if needed
 			then arg="./$arg"
 			fi
-			TEST=$(echo "$arg" | sed -E "s/(^.*)\/.*\.a$/\1/g")
+			TEST=$(echo "$arg" | sed -E "s/(^.*)\/.*\.a$/\1/g") 					# get the folder for the library
 			INCLUDES+="-I $TEST "
 			LIB+="-L $TEST "
-			TEST=$(echo "$arg" | sed -E "s/^.*\/(.*)\.a$/\1/g" | sed "s/^lib//g")
+			TEST=$(echo "$arg" | sed -E "s/^.*\/(.*)\.a$/\1/g" | sed "s/^lib//g")	# get the library name
 			LIB+="-l$TEST "
 		else echo "$arg is not an existing .a file"
 		fi
 		continue
 	fi
 done
-
-echo "\n ..VARIABLES..\n"
-
-echo "Name = '$NAME'"
-echo "CFILES = '$CFILES'"
-echo "OFILES = '$OFILES'"
-echo "LIB = '$LIB'"
-echo "INCLUDES = '$INCLUDES'"
-echo "EMAKE = '$EMAKE'"
-
 print_make
