7bc78c21f1db16387e0d82d4e4aad329e2025c6f
Petrus Hakakoski <phakakos@c1r3p2.hive.fi>
2020-03-11 19:35:47 +0200

    added POST variable existance test

--- quiz/index.php
+++ quiz/index.php
@@ -8,58 +8,58 @@ include 'top.php'; ?>
 <?php include 'questions.php';?>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if ($_POST["GO"])
+	if (isset($_POST["GO"]))
 		$_SESSION["start"] = time();
 	// PART 1
-	if ($_POST["q11"] != "")
+	if (isset($_POST["q11"]))
 		$_SESSION["q11"] = $_POST["q11"];
-	if ($_POST["q12"] != "")
+	if (isset($_POST["q12"]))
 		$_SESSION["q12"] = $_POST["q12"];
-	if ($_POST["q13"] != "")
+	if (isset($_POST["q13"]))
 		$_SESSION["q13"] = $_POST["q13"];
-	if ($_POST["q14"] != "")
+	if (isset($_POST["q14"]))
 		$_SESSION["q14"] = $_POST["q14"];
-	if ($_POST["q15"] != "")
+	if (isset($_POST["q15"]))
 		$_SESSION["q15"] = $_POST["q15"];
-	if ($_POST["q16"] != "")
+	if (isset($_POST["q16"]))
 		$_SESSION["q16"] = $_POST["q16"];
-	if ($_POST["q17"] != "")
+	if (isset($_POST["q17"]))
 		$_SESSION["q17"] = $_POST["q17"];
-	if ($_POST["q18"] != "")
+	if (isset($_POST["q18"]))
 		$_SESSION["q18"] = $_POST["q18"];
-	if ($_POST["q19"] != "")
+	if (isset($_POST["q19"]))
 		$_SESSION["q19"] = $_POST["q19"];
-	if ($_POST["q110"] != "")
+	if (isset($_POST["q110"]))
 		$_SESSION["q110"] = $_POST["q110"];
 	// PART 2
-	if ($_POST["q21"] != "")
+	if (isset($_POST["q21"]))
 		$_SESSION["q21"] = $_POST["q21"];
-	if ($_POST["q22"] != "")
+	if (isset($_POST["q22"]))
 		$_SESSION["q22"] = $_POST["q22"];
-	if ($_POST["q23"] != "")
+	if (isset($_POST["q23"]))
 		$_SESSION["q23"] = $_POST["q23"];
-	if ($_POST["q24"] != "")
+	if (isset($_POST["q24"]))
 		$_SESSION["q24"] = $_POST["q24"];
-	if ($_POST["q25"] != "")
+	if (isset($_POST["q25"]))
 		$_SESSION["q25"] = $_POST["q25"];
-	if ($_POST["q26"] != "")
+	if (isset($_POST["q26"]))
 		$_SESSION["q26"] = $_POST["q26"];
-	if ($_POST["q27"] != "")
+	if (isset($_POST["q27"]))
 		$_SESSION["q27"] = $_POST["q27"];
-	if ($_POST["q28"] != "")
+	if (isset($_POST["q28"]))
 		$_SESSION["q28"] = $_POST["q28"];
-	if ($_POST["q29"] != "")
+	if (isset($_POST["q29"]))
 		$_SESSION["q29"] = $_POST["q29"];
-	if ($_POST["q210"] != "")
+	if (isset($_POST["q210"]))
 		$_SESSION["q210"] = $_POST["q210"];
 	// PART 3
-	if ($_POST["q31"] != "")
+	if (isset($_POST["q31"]))
 		$_SESSION["q31"] = $_POST["q31"];
-	if ($_POST["q32"] != "")
+	if (isset($_POST["q32"]))
 		$_SESSION["q32"] = $_POST["q32"];
-	if ($_POST["q33"] != "")
+	if (isset($_POST["q33"]))
 		$_SESSION["q33"] = $_POST["q33"];
-	if ($_POST["name"] != "")
+	if (isset($_POST["name"]))
 		$_SESSION["name"] = htmlspecialchars($_POST["name"]);
 }
 #echo session_id();
