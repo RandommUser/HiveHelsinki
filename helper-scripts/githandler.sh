a23dd2a7890a687b1a95fe9eec304f7c43c432c0
Petrus Hakakoski <phakakos@c1r3p2.hive.fi>
2020-03-11 20:12:21 +0200

    Created single line git handler

---
+++ helper-scripts/githandler.sh
@@ -0,0 +1,46 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    githandler.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: phakakos <phakakos@student.hive.fi>        +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/03/11 19:39:53 by phakakos          #+#    #+#              #
+#    Updated: 2020/03/11 20:12:17 by phakakos         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Shell script to handle multiple git commands in a single line
+# Made by phakakos @ Hive Helsinki, 2020
+
+
+
+gitcheck(){
+# Check if in a git folder
+GIT=$(git status 2> /dev/null);
+if [[  $GIT == "" ]]
+then echo "Not in a git folder"; exit;
+fi
+}
+
+if [[ $1 == "" ]]
+then echo "usage githandler.sh (files) [-m ("'"'"commit message"'"'")] [-p (push)]"; exit
+fi
+
+mess="no";
+
+for arg in "$@"
+do
+	echo "read line $arg";
+	if [[ $mess == "yes" ]]
+	then git commit -m "$arg"; mess="no"; continue
+	elif [[ $arg == "-m" ]]
+	then mess="yes"; continue
+	elif [[ -f $arg ]]
+	then git add $arg; continue
+	elif [[ -d $arg ]]
+	then git add $arg/*; continue
+	elif [[ $arg == "-p" ]]
+	then git push; continue
+	fi
+done
