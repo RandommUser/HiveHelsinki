85797e1e07b40381874297d9957d8e0464e58ef4
Petrus Hakakoski <phakakos@c1r3p4.hive.fi>
2020-11-05 15:49:26 +0200

    Added randomness to the empty line handling. General improvements

--- helper-scripts/diff.sh
+++ helper-scripts/diff.sh
@@ -10,21 +10,36 @@
 #                                                                              #
 # **************************************************************************** #
 
-# Single line diff comparer. Excepted test output are to be one line.
-# Handles empty lines, and takes the file from the parameter
+# Single file compare for test output comparison
+# Should be added as alias or placed to the root for easy use
 # Done by phakakos, Hive Helsinki 2020
 
-# You should not have tests with expected output as "empty" or change this value
+# Example usage would be ft_printf and printf output comparison
+# Output should be formatted:
+# user output\n
+# test output\n
+# user output\n
+# test output\n
+# ...
+# Order can be switched around, the comparison will be handled the same
+# Handles empty lines
+
+# Replacement value for "" lines.
+# Uses current ISO time to make it have randomness to make it almost impossible for
+# the test cases to match the replacement
+EMPTY_LINE="empty"$(date +%s)
+
+# To compare empty output lines
 test_line()
 {
 	if [[ $1 == "" ]]
-	then echo "empty"
+	then echo "$EMPTY_LINE"
 	else echo "$line"
 	fi
 }
 
 if [[ -z $1 ]]
-then echo "usage: sh $0 'file'"; exit
+then echo "usage: $0 'file'"; exit
 elif [[ ! -f $1 ]]
 then echo "File does not exist"; exit
 fi
@@ -38,9 +53,8 @@ do
 	if [[ $COMP == "" ]]
 	then COMP="$line"
 	else
-		if [[ $COMP == $line ]]
-		then echo "test passed"
-		else echo "Diff: $COMP | $line"
+		if [[ $COMP != $line ]]
+		then echo "Diff: $COMP | $line"
 		fi
 		COMP=""
 	fi
