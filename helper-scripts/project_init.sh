078822450acf8c514d7a709675e9d0b59db92c4f
Petrus Hakakoski <phakakos@c1r3p4.hive.fi>
2020-11-05 15:52:51 +0200

    Added checks for Libft setup and variable for custom username for author. General improvements

--- helper-scripts/project_init.sh
+++ helper-scripts/project_init.sh
@@ -11,15 +11,57 @@
 # **************************************************************************** #
 
 # Script designed to be used when starting a project.
-# Should be placed in the root and can be called with ~./project_init.shi
-# or added as an alias
+# Should be added as alias or placed to the root for easy use
+# Check LIBFT and MY_USER values before starting!!!
 # Made by phakakos @ Hive Helsinki, 2019
 
 PWD=$(pwd)
 # change to your own libft folder 
 LIBFT=~/42/HiveHelsinki/libft
-LIBD=$(diff --brief libft $LIBFT)
+# By default $USER is used for the author file
+# Change this if your user does not match your intra login
+MY_USER=""
 
+write_author()
+{
+	if [[ $MY_USER == "" ]]
+	then MY_USER=$USER
+	fi
+	echo "$MY_USER" > author; echo ">Created author file"; echo '';
+}
+
+copy_lib(){
+# Test the libft folder setup is done properly
+if [[ $LIBFT == "" ]]
+then echo "LIBFT is not set. Please edit the script to have your libft path"; exit;
+elif [[ ! -d $LIBFT ]]
+then echo "LIBFT folder does not exist. Please edi the script to have your libft path"; exit;
+fi
+
+# Checks if you already have the folder and if it is up-to-date
+# Copies library from $LIBFT folder to the current folder
+if [[ -d "libft" ]]
+then
+	LIBD=$(diff --brief libft $LIBFT)
+	if [[ $LIBD == "" ]]
+		then echo "Libft up to date $LIBD"
+		elif [ -d "libft" ] ; then
+		echo "Libft already exists. Do you want to refresh it?"
+		read -p "(y/n) " lib
+		while true
+		do
+			if [[ $lib == "y" ]]
+			then make -C $LIBFT fclean; rm -r libft; cp -r $LIBFT .; echo ">Copied to current folder"; break
+			elif [[ $lib == "n" ]]
+			then echo "No re-copy"; break
+			else read -p "(y/n) " lib
+			fi
+		done
+	fi
+else
+	make -C $LIBFT fclean; cp -r $LIBFT .; echo ">Copied library to current folder"
+fi
+}
 
 main(){
 clear
@@ -32,27 +74,37 @@ do
 	if [[ $aut == "n" ]]
 	then echo "Skipping"; echo ''; break
 	elif [[ $aut == "y" ]]
-	then echo "$USER" > author; echo ">Created author file"; echo ''; break
+	then write_author; echo ''; break
 	else read -p "(y/n) " aut
 	fi
 done
 
+if [[ $LIBFT == "" ]]
+then echo "LIBFT is not set. Please edit the script to have your libft path"; exit;
+elif [[ ! -d $LIBFT ]]
+then echo "LIBFT folder does not exist. Please edi the script to have your libft path"; exit;
+fi
+
 # Do you want to export your libft folder. Checks if you already have the folder and if it is up-to-date
 # Copies library from '.libft' folder in the same folder as the script.
-if ([[ $LIBD == "" ]] && [ -d "libft" ]); then
-	echo "Libft up to date $LIBD"
-elif [ -d "libft" ] ; then
-	echo "Libft already exists. Do you want to refresh it?"
-	read -p "(y/n) " lib
-	while true
-	do
-		if [[ $lib == "y" ]]
-		then make -C $LIBFT fclean; rm -r libft; cp -r $LIBFT .; echo ">Copied to current folder"; break
-		elif [[ $lib == "n" ]]
-		then echo "No re-copy"; break
-		else read -p "(y/n) " lib
-		fi
-	done
+if [[ -d "libft" ]]
+then
+	LIBD=$(diff --brief libft $LIBFT)
+		if [[ $LIBD == "" ]]
+			then echo "Libft up to date $LIBD"
+			elif [ -d "libft" ] ; then
+			echo "Libft already exists. Do you want to refresh it?"
+			read -p "(y/n) " lib
+			while true
+		do
+			if [[ $lib == "y" ]]
+			then make -C $LIBFT fclean; rm -r libft; cp -r $LIBFT .; echo ">Copied to current folder"; break
+			elif [[ $lib == "n" ]]
+			then echo "No re-copy"; break
+			else read -p "(y/n) " lib
+			fi
+		done
+	fi
 else
 	echo "Do you want to copy libft?"
 	read -p "(y/n) " lib
@@ -71,14 +123,9 @@ echo "Exiting"
 
 # Flags to select author file or library individually
 if [[ $1 == "-a" ]]
-then echo "$USER" > author; echo ">Author edited"
+then write_author
 elif [[ $1 == "-l" ]]
-then make -C $LIBFT fclean;
-	if [[ -d "libft" ]]
-	then rm -r libft;
-	fi
-	cp -r $LIBFT .
-	echo ">Copied $LIBFT to current folder"
+then copy_lib
 elif [[ $1 != "" ]]
 	then echo "usage $0 [-a (author file)| -l (library)]"
 else
